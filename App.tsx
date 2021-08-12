d3851082d3f469936e61d6beb6c2665637067e8d
Aditya Jamuar <adityaj@geekyants.com>
2021-08-12 16:15:52 +0530

    Base string done

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
 import { NativeBaseProvider } from "native-base";
 import Home from "./screens/Home";
+/* Add Screen imports here */
 
 const Drawer = createDrawerNavigator();
 
@@ -14,7 +15,8 @@ export default function App() {
           initialRouteName="Home"
           screenOptions={{ headerShown: false }}
         >
-          <Drawer.Screen name="Home" component={Home} />
+          <Drawer.Screen name={"Home"} component={Home} />
+          {/* Add Screen components to Drawer here */}
         </Drawer.Navigator>
       </NavigationContainer>
     </NativeBaseProvider>
